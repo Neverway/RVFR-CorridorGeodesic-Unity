83ed880b7ee788a6212196aa8dd7bf6519feae03
Connorses <111316959+connor-subliminal@users.noreply.github.com>
2024-07-14 02:10:48 -0600

    Added laser object

---
+++ CorridorGeodesic/Assets/Resources/Scripts/Components/Laser_Raycast.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser_Raycast : MonoBehaviour
+{
+
+    //===================== (Neverway 2024) Written by Connorses =====================
+    //
+    // Purpose:
+    // Notes:
+    // Source:
+    //
+    //=============================================================================
+
+    private LineRenderer lineRenderer;
+    [SerializeField] private Transform halo;
+
+    private void Start ()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update ()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast (transform.position, transform.TransformDirection (Vector3.forward), out hit, Mathf.Infinity))
+        {
+            Debug.DrawRay (transform.position, transform.TransformDirection (Vector3.forward) * hit.distance, Color.red);
+            lineRenderer.SetPosition (0, transform.position);
+            lineRenderer.SetPosition (1, hit.point);
+            halo.gameObject.SetActive (true);
+            halo.transform.position = hit.point;
+        }
+        else
+        {
+            halo.gameObject.SetActive (false);
+        }
+    }
+
+}
