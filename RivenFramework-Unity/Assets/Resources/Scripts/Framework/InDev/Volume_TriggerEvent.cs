c9bc85ae86bb31efa4a08bf575fff44fc8fbb63c
lizband <lizband.dev@gmail.com>
2024-07-22 22:17:53 -0400

    Updated some triggers and stuff, Added new event and trigger volumes

--- RivenFramework-Unity/Assets/Resources/Scripts/Framework/InDev/Volume_TriggerEvent.cs
+++ RivenFramework-Unity/Assets/Resources/Scripts/Framework/InDev/Volume_TriggerEvent.cs
@@ -8,22 +8,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class Volume_Event : MonoBehaviour
+[RequireComponent(typeof(Logic_Processor))]
+public class Volume_TriggerEvent : Volume
 {
     //=-----------------=
     // Public Variables
     //=-----------------=
+    public string onOccupiedSignal;
+    public string onUnoccupiedSignal;
+    public string resetSignal;
+    public bool resetsAutomatically = true;
+    public bool checksOnlyForPlayer = true;
+    public UnityEvent onOccupied;
+    public UnityEvent onUnoccupied;
 
 
     //=-----------------=
     // Private Variables
     //=-----------------=
+    [Tooltip("A variable to keep track of if this volume has already been trigger")]
+    [HideInInspector] public bool hasBeenTriggered;
 
 
     //=-----------------=
     // Reference Variables
     //=-----------------=
+    private Logic_Processor logicProcessor;
 
 
     //=-----------------=
@@ -31,12 +43,85 @@ public class Volume_Event : MonoBehaviour
     //=-----------------=
     private void Start()
     {
-    
+        logicProcessor = GetComponent<Logic_Processor>();
     }
 
-    private void Update()
+    private new void OnTriggerEnter2D(Collider2D _other)
     {
-    
+        base.OnTriggerEnter2D(_other); // Call the base class method
+        if (!resetsAutomatically && hasBeenTriggered) return;
+        if (checksOnlyForPlayer && _other.CompareTag("Pawn"))
+        {
+            if (!targetEnt.isPossessed) return;
+            logicProcessor.UpdateState(onOccupiedSignal, true);
+            logicProcessor.UpdateState(onUnoccupiedSignal, false);
+            onOccupied.Invoke();
+        }
+        else if (pawnsInTrigger.Count != 0 || propsInTrigger.Count != 0)
+        {
+            logicProcessor.UpdateState(onOccupiedSignal, true);
+            logicProcessor.UpdateState(onUnoccupiedSignal, false);
+            onOccupied.Invoke();
+        }
+        hasBeenTriggered = true;
+    }
+
+    private new void OnTriggerExit2D(Collider2D _other)
+    {
+        base.OnTriggerExit2D(_other); // Call the base class method
+        if (checksOnlyForPlayer && _other.CompareTag("Pawn"))
+        {
+            if (!targetEnt.isPossessed) return;
+            logicProcessor.UpdateState(onOccupiedSignal, false);
+            logicProcessor.UpdateState(onUnoccupiedSignal, true);
+            onUnoccupied.Invoke();
+        }
+        else if (pawnsInTrigger.Count == 0 && propsInTrigger.Count == 0)
+        {
+            logicProcessor.UpdateState(onOccupiedSignal, false);
+            logicProcessor.UpdateState(onUnoccupiedSignal, true);
+            onUnoccupied.Invoke();
+        }
+        if (resetsAutomatically) hasBeenTriggered = false;
+    }
+
+    private new void OnTriggerEnter(Collider _other)
+    {
+        base.OnTriggerEnter(_other); // Call the base class method
+        if (!resetsAutomatically && hasBeenTriggered) return;
+        if (checksOnlyForPlayer && _other.CompareTag("Pawn"))
+        {
+            if (!targetEnt.isPossessed) return;
+            logicProcessor.UpdateState(onOccupiedSignal, true);
+            logicProcessor.UpdateState(onUnoccupiedSignal, false);
+            onOccupied.Invoke();
+        }
+        else if (pawnsInTrigger.Count != 0 || propsInTrigger.Count != 0)
+        {
+            logicProcessor.UpdateState(onOccupiedSignal, true);
+            logicProcessor.UpdateState(onUnoccupiedSignal, false);
+            onOccupied.Invoke();
+        }
+        hasBeenTriggered = true;
+    }
+
+    private new void OnTriggerExit(Collider _other)
+    {
+        base.OnTriggerExit(_other); // Call the base class method
+        if (checksOnlyForPlayer && _other.CompareTag("Pawn"))
+        {
+            if (!targetEnt.isPossessed) return;
+            logicProcessor.UpdateState(onOccupiedSignal, false);
+            logicProcessor.UpdateState(onUnoccupiedSignal, true);
+            onUnoccupied.Invoke();
+        }
+        else if (pawnsInTrigger.Count == 0 && propsInTrigger.Count == 0)
+        {
+            logicProcessor.UpdateState(onOccupiedSignal, false);
+            logicProcessor.UpdateState(onUnoccupiedSignal, true);
+            onUnoccupied.Invoke();
+        }
+        if (resetsAutomatically) hasBeenTriggered = false;
     }
 
     //=-----------------=
