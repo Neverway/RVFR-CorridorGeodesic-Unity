245340b790e766217abdb0a60f758c07114e8f5c
lizband <lizband.dev@gmail.com>
2024-08-05 02:34:21 -0400

    Added exit elevator, Changed georipper to allow non linear slicing

---
+++ CorridorGeodesic/Assets/Resources/Scripts/Project Specifics/Elevator.cs
@@ -0,0 +1,69 @@
+//===================== (Neverway 2024) Written by Liz M. =====================
+//
+// Purpose:
+// Notes:
+//
+//=============================================================================
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    //=-----------------=
+    // Public Variables
+    //=-----------------=
+    public bool isPowered;
+
+
+    //=-----------------=
+    // Private Variables
+    //=-----------------=
+    
+
+    //=-----------------=
+    // Reference Variables
+    //=-----------------=
+    [SerializeField] private Animator animator;
+
+
+    //=-----------------=
+    // Mono Functions
+    //=-----------------=
+    
+
+    //=-----------------=
+    // Internal Functions
+    //=-----------------=
+
+
+    //=-----------------=
+    // External Functions
+    //=-----------------=
+    public void OpenElevator()
+    {
+        if (isPowered) return;
+        animator.Play("Elevator_Open");
+        isPowered = true;
+    }
+    
+    public void CloseElevator()
+    {
+        if (!isPowered) return;
+        animator.Play("Elevator_Close");
+        isPowered = false;
+    }
+    
+    public void ToggleElevator()
+    {
+        if (isPowered)
+        {
+            CloseElevator();
+        }
+        else
+        {
+            OpenElevator();
+        }
+    }
+}
