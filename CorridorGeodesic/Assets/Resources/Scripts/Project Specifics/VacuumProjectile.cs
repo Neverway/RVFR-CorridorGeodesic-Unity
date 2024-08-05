f7296d88574a2f43083c2da851243ae44e9ccfcf
lizband <lizband.dev@gmail.com>
2024-08-05 02:52:28 -0400

    Fixed a bug where bulbs are not respecting layermasks

--- CorridorGeodesic/Assets/Resources/Scripts/Project Specifics/VacuumProjectile.cs
+++ CorridorGeodesic/Assets/Resources/Scripts/Project Specifics/VacuumProjectile.cs
@@ -56,7 +56,7 @@ public class VacuumProjectile : MonoBehaviour
     //=-----------------=
     private void CollisionTest()
     {
-        if (Physics.SphereCast(transform.position+castOffset, castRadius, transform.forward, out faceHit, layerMask))
+        if (Physics.SphereCast(transform.position+castOffset, castRadius, transform.forward, out faceHit, Mathf.Infinity, layerMask))
         {
             if (faceHit.collider.gameObject.TryGetComponent<AntiProjectile> (out var component))
             {
