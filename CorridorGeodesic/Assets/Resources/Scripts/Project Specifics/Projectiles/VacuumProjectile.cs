083ec26c5881be52b39d4bc09714f0793ca913a2
SoulexDev <fnafiplayar@gmail.com>
2024-08-31 05:43:37 -0700

    updated nixiecross logic

--- CorridorGeodesic/Assets/Resources/Scripts/Project Specifics/Projectiles/VacuumProjectile.cs
+++ CorridorGeodesic/Assets/Resources/Scripts/Project Specifics/Projectiles/VacuumProjectile.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class VacuumProjectile : MonoBehaviour
 {
@@ -58,11 +59,34 @@ public class VacuumProjectile : MonoBehaviour
     {
         if (Physics.SphereCast(transform.position+castOffset, castRadius, transform.forward, out faceHit, 1f, layerMask))
         {
-            if (faceHit.collider.gameObject.TryGetComponent<CorGeo_AntiProjectile> (out var component))
+            bool killScheduled = false;
+            if (faceHit.collider is MeshCollider)
             {
-                KillProjectile ();
-                return;
+                MeshCollider mCollider = (MeshCollider)faceHit.collider;
+
+                Mesh colMesh = mCollider.sharedMesh;
+
+                int triIndex = faceHit.triangleIndex;
+
+                DisplayDebugTriangle(colMesh, triIndex, faceHit.collider.transform);
+
+                if (faceHit.collider.gameObject.TryGetComponent(out Renderer rend))
+                {
+                    int subMeshIndex = GetSubMeshIndex(colMesh, triIndex);
+                    if(subMeshIndex != -1 && ReferenceManager.Instance.conductiveMats.Contains(rend.sharedMaterials[subMeshIndex]))
+                    {
+                        KillProjectile();
+                        killScheduled = true;
+                    }
+                }
             }
+            if (!killScheduled && faceHit.collider.gameObject.TryGetComponent<CorGeo_AntiProjectile> (out var component))
+            {
+                KillProjectile();
+                killScheduled = true;
+            }
+            if (killScheduled)
+                return;
 
             // Disable physics
             objectRigidbody.isKinematic = true;
@@ -75,7 +99,6 @@ public class VacuumProjectile : MonoBehaviour
             pinned = true;
         }
     }
-
     private IEnumerator Lifetime()
     {
         yield return new WaitForSeconds(1);
@@ -110,6 +133,33 @@ public class VacuumProjectile : MonoBehaviour
         
         // Erase the projectile
         Destroy (gameObject);
+    }
+    int GetSubMeshIndex(Mesh mesh, int triIndex)
+    {
+        int triangleCounter = 0;
+        for (int i = 0; i < mesh.subMeshCount; i++)
+        {
+            triangleCounter += mesh.GetSubMesh(i).indexCount / 3;
+            if (triIndex < triangleCounter)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    void DisplayDebugTriangle(Mesh mesh, int triangleIndex, Transform hitTransform)
+    {
+        Vector3[] vertices = mesh.vertices;
+        int[] triangles = mesh.triangles;
+        Vector3 p0 = vertices[triangles[triangleIndex * 3 + 0]];
+        Vector3 p1 = vertices[triangles[triangleIndex * 3 + 1]];
+        Vector3 p2 = vertices[triangles[triangleIndex * 3 + 2]];
+        p0 = hitTransform.TransformPoint(p0);
+        p1 = hitTransform.TransformPoint(p1);
+        p2 = hitTransform.TransformPoint(p2);
+        Debug.DrawLine(p0, p1, Color.red, 5);
+        Debug.DrawLine(p1, p2, Color.red, 5);
+        Debug.DrawLine(p2, p0, Color.red, 5);
     }
 
 
