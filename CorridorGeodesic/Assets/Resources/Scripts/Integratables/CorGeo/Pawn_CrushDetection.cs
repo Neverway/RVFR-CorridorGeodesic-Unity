783bee68ebbd31f8cac4b9036ba5e52aae8403b2
lizband <lizband.dev@gmail.com>
2024-09-08 15:56:06 -0400

    Cleaned up some stuff

---
+++ CorridorGeodesic/Assets/Resources/Scripts/Integratables/CorGeo/Pawn_CrushDetection.cs
@@ -0,0 +1,53 @@
+//===================== (Neverway 2024) Written by _____ =====================
+//
+// Purpose: Kills or damages a pawn if they are in too tight of a space for too long
+// Notes:
+//
+//=============================================================================
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pawn_CrushDetection: MonoBehaviour
+{
+    //=-----------------=
+    // Public Variables
+    //=-----------------=
+	
+	
+    //=-----------------=
+    // Private Variables
+    //=-----------------=
+	
+	
+    //=-----------------=
+    // Reference Variables
+    //=-----------------=
+    [SerializeField] private Pawn pawn;
+	
+	
+    //=-----------------=
+    // Mono Functions
+    //=-----------------=
+    private void Start()
+    {
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+	    print($"Colliding with {other.gameObject.name}");
+	    pawn.ModifyHealth(-5);
+    }
+
+
+    //=-----------------=
+    // Internal Functions
+    //=-----------------=
+	
+	
+    //=-----------------=
+    // External Functions
+    //=-----------------=
+}
