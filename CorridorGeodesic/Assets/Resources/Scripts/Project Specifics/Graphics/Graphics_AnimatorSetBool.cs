a2ffa8c0ac3295ce41784fc93f9d88cb66bc9b1f
SoulexDev <fnafiplayar@gmail.com>
2024-09-13 10:09:50 -0700

    little level. new floor button in progress

--- CorridorGeodesic/Assets/Resources/Scripts/Project Specifics/Graphics/Graphics_AnimatorSetBool.cs
+++ CorridorGeodesic/Assets/Resources/Scripts/Project Specifics/Graphics/Graphics_AnimatorSetBool.cs
@@ -8,6 +8,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Graphics_AnimatorSetBool: MonoBehaviour
 {
@@ -21,6 +22,7 @@ public class Graphics_AnimatorSetBool: MonoBehaviour
     //=-----------------=
     [SerializeField] private Animator anim;
     [SerializeField] private string propertyName;
+    [SerializeField] private float setBoolDelay = 0;
 	
     //=-----------------=
     // Reference Variables
@@ -37,10 +39,24 @@ public class Graphics_AnimatorSetBool: MonoBehaviour
     //=-----------------=
 	public void SetBool(bool state)
     {
+        StopAllCoroutines();
+        anim.SetBool(propertyName, state);
+    }
+    public void SetBoolDelay(bool state)
+    {
+        StartCoroutine(Delay(state));
+    }
+    IEnumerator Delay(bool state)
+    {
+        yield return new WaitForSeconds(setBoolDelay);
         anim.SetBool(propertyName, state);
     }
 	
     //=-----------------=
     // External Functions
     //=-----------------=
+}
+[System.Serializable]
+public class Graphics_AnimatorSetBoolDelayEvent : UnityEvent<bool, float>
+{
 }
