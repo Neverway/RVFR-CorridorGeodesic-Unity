c39a9e54cdd45a79f286b9f19a3aa4d82a351ea5
errynei <its.errynei@gmail.com>
2024-09-15 20:52:01 -0700

    Playing around for a bit

---
+++ CorridorGeodesic/Assets/Resources/Scripts/Project Specifics/EnableFromLogic.cs
@@ -0,0 +1,59 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnableFromLogic : LogicComponent
+{
+    //=-----------------=
+    // Public Variables
+    //=-----------------=
+    public Transform[] objectsToEnableWhenActive;
+    public Transform[] objectsToDisableWhenActive;
+
+    //=-----------------=
+    // Private Variables
+    //=-----------------=
+    [SerializeField, LogicComponentHandle] private LogicComponent inputSignal;
+
+    //=-----------------=
+    // Reference Variables
+    //=-----------------='
+    [Header("References")]
+    [HideInInspector][SerializeField] private UnityEvent onPowered;
+    [HideInInspector][SerializeField] private UnityEvent onUnpowered;
+
+
+    //=-----------------=
+    // Mono Functions
+    //=-----------------=
+    protected new void OnEnable()
+    {
+        base.OnEnable();
+        SourcePowerStateChanged(isPowered);
+    }
+    //=-----------------=
+    // Internal Functions
+    //=-----------------=
+
+    //=-----------------=
+    // External Functions
+    //=-----------------=
+    public override void AutoSubscribe()
+    {
+        subscribeLogicComponents.Add(inputSignal);
+        base.AutoSubscribe();
+    }
+    public override void SourcePowerStateChanged(bool powered)
+    {
+        isPowered = powered;
+
+        foreach(Transform t in objectsToEnableWhenActive)
+            t.gameObject.SetActive(powered);
+
+        foreach (Transform t in objectsToDisableWhenActive)
+            t.gameObject.SetActive(!powered);
+
+    }
+}
