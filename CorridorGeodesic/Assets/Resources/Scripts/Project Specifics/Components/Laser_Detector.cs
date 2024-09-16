5ae72c599ec6e80b6d5207dcd4512407b99d00ea
Connorses <onronc@gmail.com>
2024-09-16 01:55:28 -0600

    Re-add Laser_Raycast, update TestRoom

---
+++ CorridorGeodesic/Assets/Resources/Scripts/Project Specifics/Components/Laser_Detector.cs
@@ -0,0 +1,82 @@
+//===================== (Neverway 2024) Written by Liz M. =====================
+//
+// Purpose:
+// Notes:
+//
+//=============================================================================
+
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Laser_Detector : MonoBehaviour
+{
+    //=-----------------=
+    // Public Variables
+    //=-----------------=
+
+    //=-----------------=
+    // Private Variables
+    //=-----------------=
+
+    private float timeSinceHit = 0f;
+    private float timeToCancelHit = 0.1f;
+    [SerializeField] private Material offMaterial;
+    [SerializeField] private Material onMaterial;
+    [SerializeField] private GameObject laserLight;
+    private bool isActive = false;
+
+    //=-----------------=
+    // Reference Variables
+    //=-----------------=
+    [SerializeField] private Renderer laserRenderer;
+    public UnityEvent OnPowered;
+    public UnityEvent OnNotPowered;
+
+
+    //=-----------------=
+    // Mono Functions
+    //=-----------------=
+
+    private void Start ()
+    {
+        laserRenderer.material = offMaterial;
+        laserLight.SetActive(false);
+    }
+
+    private void Update ()
+    {
+        timeSinceHit += Time.deltaTime;
+        if (timeSinceHit > timeToCancelHit)//if (timeSinceHit > timeToCancelHit || meshSlicer.isCut)
+        {
+            if (isActive)
+            {
+                isActive = false;
+                laserRenderer.material = offMaterial;
+                laserLight.SetActive (false);
+                OnNotPowered.Invoke();
+            }
+        }
+    }
+
+    //=-----------------=
+    // Internal Functions
+    //=-----------------=
+
+
+    //=-----------------=
+    // External Functions
+    //=-----------------=
+
+    public void OnHit ()
+    {
+        timeSinceHit = 0f;
+        if (!isActive)
+        {
+            OnPowered.Invoke();
+            isActive = true;
+            laserRenderer.material = onMaterial;
+            laserLight.SetActive(true);
+        }
+    }
+
+}
