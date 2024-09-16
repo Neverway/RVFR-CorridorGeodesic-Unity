b74c7969602b1188da9895a1419a6c6d69eb2dec
SoulexDev <fnafiplayar@gmail.com>
2024-09-16 01:51:10 -0700

    new elevator logic. not ready yet

--- CorridorGeodesic/Assets/Resources/Scripts/Project Specifics/GameEnums.cs
+++ CorridorGeodesic/Assets/Resources/Scripts/Project Specifics/GameEnums.cs
@@ -4,3 +4,4 @@ public enum DisableType { Destroy, Disable, Event }
 public enum MusicPlayType { Play, StopAllowFadeOut, StopImmediate }
 public enum CommonStateEnum { AddForce }
 public enum PlayerStateEnum { Idle, Walk, Jump, Airborne, CrouchIdle, CrouchWalk, WallSlide }
+public enum ElevatorState { Idle, Moving }
