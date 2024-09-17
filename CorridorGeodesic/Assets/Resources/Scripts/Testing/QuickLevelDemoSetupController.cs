52500397a7a0923ee29320ddb4a43c355cc7a5af
errynei <its.errynei@gmail.com>
2024-09-17 00:00:02 -0700

    setting up demo levels

--- CorridorGeodesic/Assets/Resources/Scripts/Testing/QuickLevelDemoSetupController.cs
+++ CorridorGeodesic/Assets/Resources/Scripts/Testing/QuickLevelDemoSetupController.cs
@@ -12,6 +12,8 @@ public class QuickLevelDemoSetupController : MonoBehaviour
     public static QuickLevelDemoSetupController Instance;
 
     public QuickLevelDemoSetup levelSetup;
+    public bool destroyStreamingVolumes;
+
 
     private Volume_LevelChange levelChangeVolume;
     private PlayerStart playerStart;
@@ -22,6 +24,8 @@ public class QuickLevelDemoSetupController : MonoBehaviour
 
     public void Awake()
     {
+        DontDestroyOnLoad(gameObject);
+
         if (Instance == null)
             Instance = this;
         else
@@ -58,6 +62,15 @@ public class QuickLevelDemoSetupController : MonoBehaviour
         //levelChangeVolume = FindObjectOfType<Volume_LevelChange>();
         //playerStart = FindAnyObjectByType<PlayerStart>();
         //player = FindAnyObjectByType<Pawn>();
+
+        if (destroyStreamingVolumes)
+        {
+            Volume_LevelStreaming[] streamingVolumes = FindObjectsOfType<Volume_LevelStreaming>();
+            foreach (Volume_LevelStreaming v in streamingVolumes)
+            {
+                Destroy(v);
+            }
+        }
 
         int bigNumber = 100;
         while (levelID >= levelSetup.sceneNames.Length)
