f2fe7c816da73ebc114c1c52bd20c889c5aebdef
errynei <its.errynei@gmail.com>
2024-09-20 04:39:37 -0700

    Mostly worked on Vent fan

--- CorridorGeodesic/Assets/Resources/Scripts/Project Specifics/Components/ThrowSwitch.cs
+++ CorridorGeodesic/Assets/Resources/Scripts/Project Specifics/Components/ThrowSwitch.cs
@@ -1,18 +1,65 @@
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class ThrowSwitch : MonoBehaviour
+public class ThrowSwitch : LogicComponent
 {
-    // Start is called before the first frame update
-    void Start()
+    //=-----------------=
+    // Public Variables
+    //=-----------------=
+
+
+    //=-----------------=
+    // Private Variables
+    //=-----------------=
+    [SerializeField, LogicComponentHandle] private LogicComponent inputSignal;
+
+    //=-----------------=
+    // Reference Variables
+    //=-----------------='
+    [Header("References")]
+    [SerializeField] private Animator animator;
+    //[SerializeField] private Material poweredMaterial, unpoweredMaterial;
+    //[SerializeField] private GameObject indicatorMesh;
+    private UnityEvent onPowered;
+    private UnityEvent onUnpowered;
+
+
+    //=-----------------=
+    // Mono Functions
+    //=-----------------=
+    protected new void OnEnable()
     {
-        
+        base.OnEnable();
+        SourcePowerStateChanged(isPowered);
     }
 
-    // Update is called once per frame
-    void Update()
+    //=-----------------=
+    // Internal Functions
+    //=-----------------=
+
+    //=-----------------=
+    // External Functions
+    //=-----------------=
+    //public override void AutoSubscribe()
+    //{
+    //    subscribeLogicComponents.Add(inputSignal);
+    //    base.AutoSubscribe();
+    //}
+    public override void SourcePowerStateChanged(bool powered)
     {
-        
+        base.SourcePowerStateChanged(powered);
+
+        isPowered = powered;
+
+        if (isPowered)
+            onPowered?.Invoke();
+        else
+            onUnpowered?.Invoke();
+
+        animator.SetBool("Powered", isPowered);
     }
 }
