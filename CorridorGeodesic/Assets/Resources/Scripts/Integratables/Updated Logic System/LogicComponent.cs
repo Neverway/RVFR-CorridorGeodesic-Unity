470c2aafc95dd63787b51d19102ae8c6972ec671
Connorses <onronc@gmail.com>
2024-09-29 08:36:13 -0600

    Actually fixed elevator not opening when un-crushed

--- CorridorGeodesic/Assets/Resources/Scripts/Integratables/Updated Logic System/LogicComponent.cs
+++ CorridorGeodesic/Assets/Resources/Scripts/Integratables/Updated Logic System/LogicComponent.cs
@@ -71,9 +71,7 @@ public abstract class LogicComponent : MonoBehaviour
     {
         //Fixes Animators to update its power state when re-enabled
 
-        //todo: ask Soulex that the hell he's doing with all the subscribers
-        //I commented out this "if" because it seems unnecessary and the elevator failed this check, so the door wasn't opening.
-        //if (subscribeLogicComponents.Count > 0 && subscribeLogicComponents.TrueForAll(s => s))
+        if (subscribeLogicComponents.Count > 0 && subscribeLogicComponents.TrueForAll(s => s))
             SourcePowerStateChanged(isPowered);
 
         OnPowerStateChanged?.Invoke(isPowered);
