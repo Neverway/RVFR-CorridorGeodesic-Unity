26264d50d0801d0601aeceade20279d6fe09674b
errynei <its.errynei@gmail.com>
2024-10-09 02:49:07 -0700

    Updated Checkpoints, Organized MoreTeslas

--- CorridorGeodesic/Assets/Resources/Scripts/Project Specifics/UnparentOnAwake.cs
+++ CorridorGeodesic/Assets/Resources/Scripts/Project Specifics/UnparentOnAwake.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class UnparentOnAwake : MonoBehaviour
 {
+    [Tooltip("If checked, this component will stay parented if there is no CorGeo_ActorData or Mesh_Sliceable as a parent. " +
+        "Check this as false if you want it to always unparent")]
+    public bool onlyIfNecessaryForCorGeoActorDataOrMeshSliceable = false;
     private void Awake()
     {
+        if (onlyIfNecessaryForCorGeoActorDataOrMeshSliceable && transform.parent != null)
+        {
+            bool abortUnparent = true;
+
+            if (transform.parent.GetComponentInParent<CorGeo_ActorData>() != null)
+                abortUnparent = false;
+            if (transform.parent.GetComponentInParent<Mesh_Slicable>() != null)
+                abortUnparent = false;
+
+            if (abortUnparent)
+                return;
+        }
         transform.SetParent(null);
         Destroy(this);
     }
